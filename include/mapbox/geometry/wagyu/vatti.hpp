88c0c352aa8f1e6fecb2bb129d296ac019622920
Eric Fischer <enf@pobox.com>
2016-06-16 16:40:06 -0700

    Progress on join_points()

--- include/mapbox/geometry/wagyu/vatti.hpp
+++ include/mapbox/geometry/wagyu/vatti.hpp
@@ -441,6 +441,80 @@ int point_count(point_ptr<T> points) {
 }
 
 template <typename T>
+bool join_points(join_ptr<T> j, ring_ptr<T> outrec1, ring_ptr<T> outrec2) {
+    point_ptr<T> op1 = j->point1, op1b;
+    point_ptr<T> op2 = j->point2, op2b;
+
+    //There are 3 kinds of joins for output polygons ...
+    //1. Horizontal joins where Join.OutPt1 & Join.OutPt2 are vertices anywhere
+    //along (horizontal) collinear edges (& Join.OffPt is on the same horizontal).
+    //2. Non-horizontal joins where Join.OutPt1 & Join.OutPt2 are at the same
+    //location at the Bottom of the overlapping segment (& Join.OffPt is above).
+    //3. StrictSimple joins where edges touch but are not collinear and where
+    //Join.OutPt1, Join.OutPt2 & Join.OffPt all share the same point.
+
+    bool is_horizontal = (j->point1->y == j->off_point.y);
+
+    if (is_horizontal && (j->off_point == *j->point1) && (j->off_point == *j->point2)) {
+        if (outrec1 == outrec2) {
+            // First, op1 next and op2 prev
+
+            op1b = j->point1->next;
+            while (op1b != op1 && *op1b == j->off_point) {
+                op1b = op1b->next;
+            }
+
+            op2b = j->point2->prev;
+            while (op2b != op2 && *op2b == j->off_point) {
+                op2b = op2b->prev;
+            }
+
+            if (*op1b == *op2b) {
+                point_ptr<T> op1b_prev = op1b->prev;
+                point_ptr<T> op2b_next = op2b->next;
+
+                op1b->prev = op2b;
+                op2b->next = op1b;
+
+                op1b_prev->next = op2b_next;
+                op2b_next->prev = op1b_prev;
+                return true;
+            }
+
+            // Second, op1 prev and op2 next
+
+            op2b = j->point2->next;
+            while (op2b != op2 && *op2b == j->off_point) {
+                op2b = op2b->next;
+            }
+
+            op1b = j->point1->prev;
+            while (op1b != op1 && *op1b == j->off_point) {
+                op1b = op1b->prev;
+            }
+
+            if (*op2b == *op1b) {
+                point_ptr<T> op2b_prev = op2b->prev;
+                point_ptr<T> op1b_next = op1b->next;
+
+                op2b->prev = op1b;
+                op1b->next = op2b;
+
+                op2b_prev->next = op1b_next;
+                op1b_next->prev = op2b_prev;
+                return true;
+            }
+
+            return false;
+        }
+
+        // XXX ENF left off here
+    }
+
+    return true; // XXX
+}
+
+template <typename T>
 void join_common_edges(join_list<T>& joins, ring_list<T>& rings) {
     for (size_t i = 0; i < joins.size(); i++) {
         join_ptr<T> join = &joins[i];
@@ -469,12 +543,9 @@ void join_common_edges(join_list<T>& joins, ring_list<T>& rings) {
             hole_state_ring = get_lower_most_ring(ring1, ring2);
         }
 
-// XXX ENF left off here
-#if 0
         if (!join_points(join, ring1, ring2)) {
             continue;
         }
-#endif
 
         if (ring1 == ring2) {
             // Instead of joining two polygons, we have created a new one
