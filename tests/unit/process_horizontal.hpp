3618c6d586b148a40dd69bfc787ab0645f9db1a7
Tim Channell <tim.channell@gmail.com>
2016-06-16 10:51:53 -0700

    test get_horizontal_direction

---
+++ tests/unit/process_horizontal.hpp
@@ -0,0 +1,32 @@
+#include "catch.hpp"
+
+#include <mapbox/geometry/wagyu/process_horizontal.hpp>
+
+using namespace mapbox::geometry::wagyu;
+using T = std::int64_t;
+
+TEST_CASE("get_horizontal_direction finds edge direction") {
+    mapbox::geometry::point<T> p1 = { 0, 5 };
+    mapbox::geometry::point<T> p2 = { 0, 7 };
+    edge<T> e1(p1, p2, polygon_type_subject);
+
+    T left;
+    T right;
+    horizontal_direction dir;
+
+    process_horizontal(e1, dir, left, right);
+
+    CHECK(dir == horizontal_direction::left_to_right);
+    CHECK(left == p1.x);
+    CHECK(right == p2.x);
+
+
+    // flip horizontal direction
+    edge<T> e2(p2, p1, polygon_type_subject);
+
+    process_horizontal(e2, dir, left, right);
+
+    CHECK(dir == horizontal_direction::right_to_left);
+    CHECK(left == p2.x);
+    CHECK(right == p1.x);
+};
