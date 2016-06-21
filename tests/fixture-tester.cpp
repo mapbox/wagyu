d24e81101e270267863ed8626082785f83d3eb0c
Tim Channell <tim.channell@gmail.com>
2016-06-21 16:11:03 -0700

    fixture-tester reads and outputs json

--- tests/fixture-tester.cpp
+++ tests/fixture-tester.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <mapbox/geometry/polygon.hpp>
 #include <rapidjson/document.h>
+#include "rapidjson/writer.h"
 #include <rapidjson/filereadstream.h>
+#include <rapidjson/filewritestream.h>
 #include <mapbox/geometry/wagyu/wagyu.hpp>
 
 
@@ -16,14 +18,15 @@ void parse_file(const char* file_path,
     clipper<value_type>& clipper, 
     polygon_type polytype
 ) {
+    // todo safety checks opening files
     FILE* file = fopen(file_path, "r");
     char read_buffer[65536];
     FileReadStream in_stream(file, read_buffer, sizeof(read_buffer));
     Document document;
     document.ParseStream<0, UTF8<>, FileReadStream>(in_stream);
 
+    // todo catch parsing errors
 
-    std::cout << document.IsArray() << std::endl;
     for (SizeType i = 0; i < document.Size(); ++i) {
         mapbox::geometry::linear_ring<value_type> lr;
 
@@ -32,6 +35,8 @@ void parse_file(const char* file_path,
         } 
         clipper.add_ring(lr, polytype);
     }
+
+    fclose(file);
 }
 
 int main(int argc, char* const argv[]) {
@@ -42,8 +47,40 @@ int main(int argc, char* const argv[]) {
     parse_file(argv[1], clipper, polygon_type_subject);
     parse_file(argv[1], clipper, polygon_type_clip);
 
-
     std::vector<mapbox::geometry::polygon<value_type>> solution;
     clipper.execute(clip_type_union, solution, fill_type_even_odd, fill_type_even_odd);
+
+
+    Document output;
+    output.SetArray();
+    Document::AllocatorType& allocator = output.GetAllocator();
+    output.Reserve(solution.size(), allocator);
+
+    // Polygons
+    for (std::size_t p = 0; p < solution.size(); ++p) {
+        output.PushBack(Value().SetArray(), allocator);
+        output[p].Reserve(solution[p].size(), allocator);
+
+        // Rings
+        for (std::size_t r = 0; r < solution[p].size(); ++r) {
+            output[p].PushBack(Value().SetArray(), allocator);
+            output[p][r].Reserve(solution[p][r].size(), allocator);
+
+            // Coordinates
+            for (auto coord : solution[p][r]) {
+                Value cvalue;
+                cvalue.SetArray();
+                cvalue.PushBack(Value().SetInt(coord.x), allocator);
+                cvalue.PushBack(Value().SetInt(coord.y), allocator);
+                output[p][r].PushBack(cvalue, allocator);
+            }
+        }
+    }
+
+    char write_buffer[65536];
+    FileWriteStream out_stream(stdout, write_buffer, sizeof(write_buffer));
+    Writer<FileWriteStream> writer(out_stream);
+
+    output.Accept(writer);
 }
 
