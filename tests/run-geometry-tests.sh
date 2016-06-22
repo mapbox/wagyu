eebe8e04d3031c3476e5bf28e552d6af9b3d00aa
jakepruitt <jake@mapbox.com>
2016-06-22 15:25:44 -0700

    Remove semicolon

--- tests/run-geometry-tests.sh
+++ tests/run-geometry-tests.sh
@@ -1,4 +1,4 @@
-for filename in $(ls ./tests/geometry-test-data/input-polyjson):
+for filename in $(ls ./tests/geometry-test-data/input-polyjson)
 do
     echo $filename
     ./fixture-tester ./tests/fixtures/clip-clockwise-square.json ./tests/geometry-test-data/input-polyjson/$filename > ./tests/output-polyjson/$filename
