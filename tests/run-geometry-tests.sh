35c07906a04f55a1c06a86905cfba28e9e548f57
Tim Channell <tim.channell@gmail.com>
2016-06-23 13:47:54 -0700

    add error output, failure/pass counts, catch missing fixture-tester argument

--- tests/run-geometry-tests.sh
+++ tests/run-geometry-tests.sh
@@ -1,12 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # ./tests/run-geometry-tests.sh ./fixture-tester
+TESTER="$1"
+PASSES=0
+FAILS=0
+
+if [ -z "$TESTER" ]; then
+    echo "Error: path to a fixture-tester binary must be supplied"
+    exit 1
+elif [ ! -e "$TESTER" ]; then
+    echo "Error: path to fixture-tester binary is invalid"
+    exit 1
+fi
+
+
+mkdir -p ./tests/output-polyjson
 
 for filename in $(ls ./tests/geometry-test-data/input-polyjson)
 do
     for type in union difference x_or intersection
     do
-        echo $type $filename
-        $1 -t $type ./tests/fixtures/clip-clockwise-square.json ./tests/geometry-test-data/input-polyjson/$filename > ./tests/output-polyjson/$type-$filename
+        $TESTER -t $type \
+            ./tests/fixtures/clip-clockwise-square.json \
+            ./tests/geometry-test-data/input-polyjson/$filename \
+            1>./tests/output-polyjson/$type-$filename;
+
+        # Check exit code of last command
+        if [ "$?" -eq "0" ]; then
+            PASSES=$((PASSES + 1))
+        else
+            echo $type $filename
+            FAILS=$((FAILS + 1))
+        fi
     done
 done
+
+TOTAL=$((PASSES + FAILS))
+echo -e "\033[1;32m ✓ $PASSES/$TOTAL \033[0;31m ✗ $FAILS/$TOTAL \033[0m"
+if [ "$FAILS" -gt "1" ]; then 
+    exit 1;
+fi
+
