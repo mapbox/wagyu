97b6f48a7b0673aeb89c4cc2770daf36795cb8c4
Blake Thompson <flippmoke@gmail.com>
2016-11-01 16:27:31 -0500

    Better estimation of allocation sizes

--- include/mapbox/geometry/wagyu/snap_rounding.hpp
+++ include/mapbox/geometry/wagyu/snap_rounding.hpp
@@ -165,6 +165,7 @@ void build_hot_pixels(local_minimum_list<T>& minima_list,
         process_hot_pixel_edges_at_top_of_scanbeam(scanline_y, scanbeam, active_bounds, rings);
         
     }
+    rings.storage_max_size = rings.hot_pixels.size();
     sort_hot_pixels(rings);
 }
 
