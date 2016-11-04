6ac24e55b895040b9fbd449ecbe3238a023da3b1
Blake Thompson <flippmoke@gmail.com>
2016-11-04 13:18:28 -0500

    Prefer a deque over a list

--- include/mapbox/geometry/wagyu/local_minimum.hpp
+++ include/mapbox/geometry/wagyu/local_minimum.hpp
@@ -27,7 +27,7 @@ struct local_minimum {
 };
 
 template <typename T>
-using local_minimum_list = std::list<local_minimum<T>>;
+using local_minimum_list = std::deque<local_minimum<T>>;
 
 template <typename T>
 using local_minimum_itr = typename local_minimum_list<T>::iterator;
