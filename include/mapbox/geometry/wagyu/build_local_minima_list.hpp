7eb76c778ae6db62b27d54724badf8a077c189f2
Blake Thompson <flippmoke@gmail.com>
2016-11-04 12:05:46 -0500

    Added some reserves to vectors before they are built

--- include/mapbox/geometry/wagyu/build_local_minima_list.hpp
+++ include/mapbox/geometry/wagyu/build_local_minima_list.hpp
@@ -14,6 +14,7 @@ bool add_line_string(mapbox::geometry::line_string<T> const& path_geometry,
                      local_minimum_list<T>& minima_list) {
     bool is_flat = true;
     edge_list<T> new_edges;
+    new_edges.reserve(path_geometry.size());
     if (!build_edge_list(path_geometry, new_edges, is_flat) || new_edges.empty()) {
         return false;
     }
@@ -26,6 +27,7 @@ bool add_linear_ring(mapbox::geometry::linear_ring<T> const& path_geometry,
                      local_minimum_list<T>& minima_list,
                      polygon_type p_type) {
     edge_list<T> new_edges;
+    new_edges.reserve(path_geometry.size());
     if (!build_edge_list(path_geometry, new_edges) || new_edges.empty()) {
         return false;
     }
