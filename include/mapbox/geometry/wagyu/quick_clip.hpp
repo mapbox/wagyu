7909e35c2732ac168796d7642ff055409aea612b
Eric Fischer <enf@pobox.com>
2017-01-09 11:42:02 -0800

    Make indent

--- include/mapbox/geometry/wagyu/quick_clip.hpp
+++ include/mapbox/geometry/wagyu/quick_clip.hpp
@@ -17,12 +17,11 @@ namespace quick_clip {
 
 template <typename T>
 bool point_inside(mapbox::geometry::point<T> const& pt, mapbox::geometry::box<T> const& b) {
-    return (pt.x >= b.min.x && pt.x <= b.max.x &&
-            pt.y >= b.min.y && pt.y <= b.max.y);
+    return (pt.x >= b.min.x && pt.x <= b.max.x && pt.y >= b.min.y && pt.y <= b.max.y);
 }
 
 template <typename T>
-void add_point(mapbox::geometry::linear_ring<T> & ring, mapbox::geometry::point<T> const& pt) {
+void add_point(mapbox::geometry::linear_ring<T>& ring, mapbox::geometry::point<T> const& pt) {
     if (ring.empty() || ring.back() != pt) {
         ring.push_back(pt);
     }
@@ -34,9 +33,9 @@ void compute_intersection_x(T x,
                             T max_y,
                             mapbox::geometry::point<T> const& pt1,
                             mapbox::geometry::point<T> const& pt2,
-                            mapbox::geometry::linear_ring<T> & new_pts) {
+                            mapbox::geometry::linear_ring<T>& new_pts) {
     // Since we are dealing with x constant value, this is
-    // an intersection with a vertical line. Therefore, if 
+    // an intersection with a vertical line. Therefore, if
     // dy == 0, this is another vertial line and therefore they
     // can not intersect
     T dy = pt2.y - pt1.y;
@@ -47,7 +46,8 @@ void compute_intersection_x(T x,
     T pt_min_y = std::min(pt2.y, pt1.y);
     T pt_max_y = std::max(pt2.y, pt1.y);
     double dydx = static_cast<double>(dy) / static_cast<double>(dx);
-    T y = static_cast<T>(std::round(static_cast<double>(pt1.y) + dydx * static_cast<double>(x - pt1.x)));
+    T y = static_cast<T>(
+        std::round(static_cast<double>(pt1.y) + dydx * static_cast<double>(x - pt1.x)));
     if (y >= pt_min_y && y <= pt_max_y) {
         // There is an interception with the line during within this segment.
         // So lets add it to the new_pts
@@ -66,9 +66,9 @@ void compute_intersection_y(T y,
                             T max_x,
                             mapbox::geometry::point<T> const& pt1,
                             mapbox::geometry::point<T> const& pt2,
-                            mapbox::geometry::linear_ring<T> & new_pts) {
+                            mapbox::geometry::linear_ring<T>& new_pts) {
     // Since we are dealing with x constant value, this is
-    // an intersection with a horizontal line. Therefore, if 
+    // an intersection with a horizontal line. Therefore, if
     // dx == 0, this is another horizontal line and therefore they
     // can not intersect
     T dx = pt2.x - pt1.x;
@@ -79,7 +79,8 @@ void compute_intersection_y(T y,
     T pt_min_x = std::min(pt2.x, pt1.x);
     T pt_max_x = std::max(pt2.x, pt1.x);
     double dxdy = static_cast<double>(dx) / static_cast<double>(dy);
-    T x = static_cast<T>(std::round(static_cast<double>(pt1.x) + dxdy * static_cast<double>(y - pt1.y)));
+    T x = static_cast<T>(
+        std::round(static_cast<double>(pt1.x) + dxdy * static_cast<double>(y - pt1.y)));
     if (x >= pt_min_x && x <= pt_max_x) {
         // There is an interception with the line during within this segment.
         // So lets add it to the new_pts
@@ -93,7 +94,7 @@ void compute_intersection_y(T y,
 }
 
 template <typename T>
-void add_intersection_point(mapbox::geometry::linear_ring<T> & ring,
+void add_intersection_point(mapbox::geometry::linear_ring<T>& ring,
                             mapbox::geometry::box<T> const& b,
                             mapbox::geometry::point<T> const& pt1,
                             mapbox::geometry::point<T> const& pt2) {
@@ -116,27 +117,27 @@ void add_intersection_point(mapbox::geometry::linear_ring<T> & ring,
     }
     if (dx > 0) {
         // dx positive means we sort min to max
-        std::sort(new_pts.begin(), new_pts.end(), 
-                [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
-                    return p1.x < p2.x;
-                });
+        std::sort(new_pts.begin(), new_pts.end(),
+                  [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
+                      return p1.x < p2.x;
+                  });
     } else if (dx < 0) {
-        std::sort(new_pts.begin(), new_pts.end(), 
-                [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
-                    return p1.x > p2.x;
-                });
+        std::sort(new_pts.begin(), new_pts.end(),
+                  [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
+                      return p1.x > p2.x;
+                  });
     } else if (dy > 0) {
         // Because dx == 0 we fall back to dy for sorting
-        std::sort(new_pts.begin(), new_pts.end(), 
-                [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
-                    return p1.y < p2.y;
-                });
+        std::sort(new_pts.begin(), new_pts.end(),
+                  [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
+                      return p1.y < p2.y;
+                  });
     } else {
         // Because dx == 0 we fall back to dy for sorting
-        std::sort(new_pts.begin(), new_pts.end(), 
-                [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
-                    return p1.y > p2.y;
-                });
+        std::sort(new_pts.begin(), new_pts.end(),
+                  [](mapbox::geometry::point<T> const& p1, mapbox::geometry::point<T> const& p2) {
+                      return p1.y > p2.y;
+                  });
     }
     for (auto const& pt : new_pts) {
         add_point(ring, pt);
@@ -175,13 +176,12 @@ optional_linear_ring<T> quick_lr_clip(mapbox::geometry::linear_ring<T> const& ri
     }
     return optional_linear_ring<T>(std::move(new_ring));
 }
-
 }
 
 template <typename T>
 mapbox::geometry::multi_polygon<T> clip(mapbox::geometry::polygon<T> const& poly,
-                               mapbox::geometry::box<T> const& b,
-                               fill_type subject_fill_type) {
+                                        mapbox::geometry::box<T> const& b,
+                                        fill_type subject_fill_type) {
     mapbox::geometry::multi_polygon<T> result;
     wagyu<T> clipper;
     for (auto const& lr : poly) {
@@ -196,8 +196,8 @@ mapbox::geometry::multi_polygon<T> clip(mapbox::geometry::polygon<T> const& poly
 
 template <typename T>
 mapbox::geometry::multi_polygon<T> clip(mapbox::geometry::multi_polygon<T> const& mp,
-                               mapbox::geometry::box<T> const& b,
-                               fill_type subject_fill_type) {
+                                        mapbox::geometry::box<T> const& b,
+                                        fill_type subject_fill_type) {
     mapbox::geometry::multi_polygon<T> result;
     wagyu<T> clipper;
     for (auto const& poly : mp) {
@@ -211,7 +211,6 @@ mapbox::geometry::multi_polygon<T> clip(mapbox::geometry::multi_polygon<T> const
     clipper.execute(clip_type_union, result, subject_fill_type, fill_type_even_odd);
     return result;
 }
-
 }
 }
 }
