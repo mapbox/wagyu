48745f95e650285ab5394d4f8c42971ffd0a09ce
Eric Fischer <enf@pobox.com>
2017-01-09 14:46:02 -0800

    Weird results when three successive points are outside the clip

--- tests/unit/quick_clip.cpp
+++ tests/unit/quick_clip.cpp
@@ -89,3 +89,102 @@ TEST_CASE("square cut at left") {
 
     CHECK(*out == lr2);
 }
+
+TEST_CASE("square cut at top") {
+    mapbox::geometry::point<T> p1 = { 0, 0 };
+    mapbox::geometry::point<T> p2 = { 100, 100 };
+    mapbox::geometry::box<T> bbox(p1, p2);
+
+    mapbox::geometry::linear_ring<T> lr;
+    lr.push_back(mapbox::geometry::point<T>(25, 25));
+    lr.push_back(mapbox::geometry::point<T>(75, 25));
+    lr.push_back(mapbox::geometry::point<T>(75, 175));
+    lr.push_back(mapbox::geometry::point<T>(25, 175));
+    lr.push_back(mapbox::geometry::point<T>(25, 25));
+
+    optional_linear_ring<T> out = mapbox::geometry::wagyu::quick_clip::quick_lr_clip(lr, bbox);
+
+    mapbox::geometry::linear_ring<T> lr2;
+    lr2.push_back(mapbox::geometry::point<T>(25, 25));
+    lr2.push_back(mapbox::geometry::point<T>(75, 25));
+    lr2.push_back(mapbox::geometry::point<T>(75, 100));
+    lr2.push_back(mapbox::geometry::point<T>(25, 100));
+    lr2.push_back(mapbox::geometry::point<T>(25, 25));
+
+    CHECK(*out == lr2);
+}
+
+TEST_CASE("square cut at bottom") {
+    mapbox::geometry::point<T> p1 = { 0, 0 };
+    mapbox::geometry::point<T> p2 = { 100, 100 };
+    mapbox::geometry::box<T> bbox(p1, p2);
+
+    mapbox::geometry::linear_ring<T> lr;
+    lr.push_back(mapbox::geometry::point<T>(-25, 25));
+    lr.push_back(mapbox::geometry::point<T>(75, 25));
+    lr.push_back(mapbox::geometry::point<T>(75, 75));
+    lr.push_back(mapbox::geometry::point<T>(-25, 75));
+    lr.push_back(mapbox::geometry::point<T>(-25, 25));
+
+    optional_linear_ring<T> out = mapbox::geometry::wagyu::quick_clip::quick_lr_clip(lr, bbox);
+
+    mapbox::geometry::linear_ring<T> lr2;
+    lr2.push_back(mapbox::geometry::point<T>(0, 25));
+    lr2.push_back(mapbox::geometry::point<T>(75, 25));
+    lr2.push_back(mapbox::geometry::point<T>(75, 75));
+    lr2.push_back(mapbox::geometry::point<T>(0, 75));
+    lr2.push_back(mapbox::geometry::point<T>(0, 25));
+
+    CHECK(*out == lr2);
+}
+
+TEST_CASE("square cut at top right") {
+    mapbox::geometry::point<T> p1 = { 0, 0 };
+    mapbox::geometry::point<T> p2 = { 100, 100 };
+    mapbox::geometry::box<T> bbox(p1, p2);
+
+    mapbox::geometry::linear_ring<T> lr;
+    lr.push_back(mapbox::geometry::point<T>(25, 25));
+    lr.push_back(mapbox::geometry::point<T>(175, 25));
+    lr.push_back(mapbox::geometry::point<T>(175, 175));
+    lr.push_back(mapbox::geometry::point<T>(25, 175));
+    lr.push_back(mapbox::geometry::point<T>(25, 25));
+
+    optional_linear_ring<T> out = mapbox::geometry::wagyu::quick_clip::quick_lr_clip(lr, bbox);
+
+    mapbox::geometry::linear_ring<T> lr2;
+    lr2.push_back(mapbox::geometry::point<T>(25, 25));
+    lr2.push_back(mapbox::geometry::point<T>(100, 25));
+    lr2.push_back(mapbox::geometry::point<T>(100, 100));
+    lr2.push_back(mapbox::geometry::point<T>(25, 100));
+    lr2.push_back(mapbox::geometry::point<T>(25, 25));
+
+    CHECK(*out == lr2);
+}
+
+TEST_CASE("square cut at top and bottom right") {
+    mapbox::geometry::point<T> p1 = { 0, 0 };
+    mapbox::geometry::point<T> p2 = { 100, 100 };
+    mapbox::geometry::box<T> bbox(p1, p2);
+
+    mapbox::geometry::linear_ring<T> lr;
+    lr.push_back(mapbox::geometry::point<T>(25, -25));
+    lr.push_back(mapbox::geometry::point<T>(175, -25));
+    lr.push_back(mapbox::geometry::point<T>(175, 175));
+    lr.push_back(mapbox::geometry::point<T>(25, 175));
+    lr.push_back(mapbox::geometry::point<T>(25, -25));
+
+    optional_linear_ring<T> out = mapbox::geometry::wagyu::quick_clip::quick_lr_clip(lr, bbox);
+
+    mapbox::geometry::linear_ring<T> lr2;
+    lr2.push_back(mapbox::geometry::point<T>(25, 0));
+    lr2.push_back(mapbox::geometry::point<T>(100, 0));
+    lr2.push_back(mapbox::geometry::point<T>(100, 100));
+    lr2.push_back(mapbox::geometry::point<T>(25, 100));
+    lr2.push_back(mapbox::geometry::point<T>(25, 0));
+
+    std::cerr << "want " << lr2 << "\n";
+    std::cerr << "got  " << *out << "\n";
+
+    CHECK(*out == lr2);
+}
