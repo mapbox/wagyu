edfc2b67f93b7f76d71537f5ed50ec59b027185f
Eric Fischer <enf@pobox.com>
2017-01-10 16:31:06 -0800

    Test that a ring that sticks out and back in becomes two squares

--- tests/unit/quick_clip.cpp
+++ tests/unit/quick_clip.cpp
@@ -226,3 +226,63 @@ TEST_CASE("square entirely enclosing bbox") {
 
     CHECK(*out == want);
 }
+
+TEST_CASE("sticking out and back in") {
+    mapbox::geometry::point<T> p1 = { 0, 0 };
+    mapbox::geometry::point<T> p2 = { 100, 100 };
+    mapbox::geometry::box<T> bbox(p1, p2);
+
+    mapbox::geometry::linear_ring<T> lr;
+    lr.push_back(mapbox::geometry::point<T>(25, 25));
+    lr.push_back(mapbox::geometry::point<T>(150, 25));
+    lr.push_back(mapbox::geometry::point<T>(150, 150));
+    lr.push_back(mapbox::geometry::point<T>(25, 150));
+    lr.push_back(mapbox::geometry::point<T>(25, 90));
+    lr.push_back(mapbox::geometry::point<T>(75, 90));
+    lr.push_back(mapbox::geometry::point<T>(75, 125));
+    lr.push_back(mapbox::geometry::point<T>(125, 125));
+    lr.push_back(mapbox::geometry::point<T>(125, 75));
+    lr.push_back(mapbox::geometry::point<T>(25, 75));
+    lr.push_back(mapbox::geometry::point<T>(25, 25));
+
+    optional_linear_ring<T> out = mapbox::geometry::wagyu::quick_clip::quick_lr_clip(lr, bbox);
+
+    mapbox::geometry::linear_ring<T> want;
+    want.push_back(mapbox::geometry::point<T>(25, 25));
+    want.push_back(mapbox::geometry::point<T>(100, 25));
+    want.push_back(mapbox::geometry::point<T>(100, 100));
+    want.push_back(mapbox::geometry::point<T>(25, 100));
+    want.push_back(mapbox::geometry::point<T>(25, 90));
+    want.push_back(mapbox::geometry::point<T>(75, 90));
+    want.push_back(mapbox::geometry::point<T>(75, 100));
+    want.push_back(mapbox::geometry::point<T>(100, 100));
+    want.push_back(mapbox::geometry::point<T>(100, 75));
+    want.push_back(mapbox::geometry::point<T>(25, 75));
+    want.push_back(mapbox::geometry::point<T>(25, 25));
+
+    CHECK(*out == want);
+
+    mapbox::geometry::wagyu::wagyu<T> clipper;
+    clipper.add_ring(*out);
+    mapbox::geometry::multi_polygon<T> solution;
+    clipper.execute(clip_type_union, solution, fill_type_even_odd, fill_type_even_odd);
+
+    CHECK(solution.size() == 2);
+
+    mapbox::geometry::linear_ring<T> want0;
+    want0.push_back(mapbox::geometry::point<T>(75, 90));
+    want0.push_back(mapbox::geometry::point<T>(75, 100));
+    want0.push_back(mapbox::geometry::point<T>(25, 100));
+    want0.push_back(mapbox::geometry::point<T>(25, 90));
+    want0.push_back(mapbox::geometry::point<T>(75, 90));
+
+    mapbox::geometry::linear_ring<T> want1;
+    want1.push_back(mapbox::geometry::point<T>(100, 25));
+    want1.push_back(mapbox::geometry::point<T>(100, 75));
+    want1.push_back(mapbox::geometry::point<T>(25, 75));
+    want1.push_back(mapbox::geometry::point<T>(25, 25));
+    want1.push_back(mapbox::geometry::point<T>(100, 25));
+
+    CHECK(solution[0][0] == want0);
+    CHECK(solution[1][0] == want1);
+}
