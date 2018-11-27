4fcf879d45731705ba0691cdc8b3bc33d3f62d53
Blake Thompson <flippmoke@gmail.com>
2018-11-27 11:03:12 -0600

    Another small fix

--- fuzzer/main.cpp
+++ fuzzer/main.cpp
@@ -18,7 +18,7 @@ static void signal_handler(int value) {
 }
 
 static void catch_signals() {
-    struct sigaction action{};
+    struct sigaction action; // NOLINT
     action.sa_handler = signal_handler;
     action.sa_flags = 0;
     sigemptyset(&action.sa_mask);
--- tests/util/fixture_utils.hpp
+++ tests/util/fixture_utils.hpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <iostream>
-#include <stdexcept>
 #include <mapbox/geometry/polygon.hpp>
+#include <stdexcept>
 
 #include <rapidjson/document.h>
 #include <rapidjson/filereadstream.h>
