c96e9e2702dea2e682366d3c5feb56cfffa15ff5
Raul Marin <git@rmr.ninja>
2018-12-28 16:58:14 +0100

    Scanbeam: Use set instead of vector

--- include/mapbox/geometry/wagyu/scanbeam.hpp
+++ include/mapbox/geometry/wagyu/scanbeam.hpp
@@ -4,23 +4,24 @@
 #include <mapbox/geometry/wagyu/local_minimum.hpp>
 
 #include <algorithm>
+#include <iterator>
 
 namespace mapbox {
 namespace geometry {
 namespace wagyu {
 
 template <typename T>
-using scanbeam_list = std::vector<T>;
+using scanbeam_list = std::set<T>;
 
 template <typename T>
 bool pop_from_scanbeam(T& Y, scanbeam_list<T>& scanbeam) {
     if (scanbeam.empty()) {
         return false;
     }
-    std::sort(scanbeam.begin(), scanbeam.end());
-    scanbeam.erase(std::unique(scanbeam.begin(), scanbeam.end()), scanbeam.end());
-    Y = scanbeam.back();
-    scanbeam.pop_back();
+
+    auto back = std::prev(scanbeam.end());
+    Y = *back;
+    scanbeam.erase(back);
     return true;
 }
 
@@ -28,7 +29,7 @@ template <typename T>
 void setup_scanbeam(local_minimum_list<T>& minima_list, scanbeam_list<T>& scanbeam) {
 
     for (auto lm = minima_list.begin(); lm != minima_list.end(); ++lm) {
-        scanbeam.push_back(lm->y);
+        scanbeam.insert(lm->y);
     }
 }
 } // namespace wagyu
