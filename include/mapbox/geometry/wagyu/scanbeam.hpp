2c61783883b19fb2cfd5d2638b4a8c5f0a9dd4aa
Raul Marin <git@rmr.ninja>
2019-01-04 13:06:08 +0100

    Scanbeam: Keep the polygon sorted to avoid sort calls

--- include/mapbox/geometry/wagyu/scanbeam.hpp
+++ include/mapbox/geometry/wagyu/scanbeam.hpp
@@ -4,14 +4,21 @@
 #include <mapbox/geometry/wagyu/local_minimum.hpp>
 
 #include <algorithm>
-#include <iterator>
 
 namespace mapbox {
 namespace geometry {
 namespace wagyu {
 
 template <typename T>
-using scanbeam_list = std::set<T>;
+using scanbeam_list = std::vector<T>;
+
+template <typename T>
+void insert_sorted_scanbeam(scanbeam_list<T>& scanbeam, T& t) {
+    typename scanbeam_list<T>::iterator i = std::lower_bound(scanbeam.begin(), scanbeam.end(), t);
+    if (i == scanbeam.end() || t < *i) {
+        scanbeam.insert(i, t);
+    }
+}
 
 template <typename T>
 bool pop_from_scanbeam(T& Y, scanbeam_list<T>& scanbeam) {
@@ -19,18 +26,19 @@ bool pop_from_scanbeam(T& Y, scanbeam_list<T>& scanbeam) {
         return false;
     }
 
-    auto back = std::prev(scanbeam.end());
-    Y = *back;
-    scanbeam.erase(back);
+    Y = scanbeam.back();
+    scanbeam.pop_back();
     return true;
 }
 
 template <typename T>
 void setup_scanbeam(local_minimum_list<T>& minima_list, scanbeam_list<T>& scanbeam) {
 
+    scanbeam.reserve(minima_list.size());
     for (auto lm = minima_list.begin(); lm != minima_list.end(); ++lm) {
-        scanbeam.insert(lm->y);
+        scanbeam.push_back(lm->y);
     }
+    std::sort(scanbeam.begin(), scanbeam.end());
 }
 } // namespace wagyu
 } // namespace geometry
